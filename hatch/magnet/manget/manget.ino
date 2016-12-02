255ba463a4b1c327ea67852900914ff84a513430
TORICA <torica@ed.tus.ac.jp>
2016-12-02 18:30:29 +0900

    magnet sen test

---
+++ hatch/magnet/manget/manget.ino
@@ -0,0 +1,21 @@
+//manget.ino
+/*
+ホール素子HG-166Aの信号をアナログピンに直接接続
+部室にある秋月のダイナミックスピーカを近づけてみたが1~3LSBほどしか変化がなかった。
+また、極性も区別できない。
+*/
+
+const int SEN=0;
+
+int val=0;
+
+void setup() {
+  // put your setup code here, to run once:
+	Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+	val=analogRead(SEN);
+	Serial.println(val);
+}
