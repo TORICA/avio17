b599186f78e606a92fecb9b8731ccd054ac3f603
tPublic <tPublic>
2016-12-07 20:17:09 +0900

    test

---
+++ tailcomp/test/test.ino
@@ -0,0 +1,11 @@
+//test.ino
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("aaa");
+}
