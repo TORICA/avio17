8176d309a67cfdb5a063a0d533a6c9cc3935f4f7
pzpz11 <kings11masakazu@gmail.com>
2017-03-15 18:10:07 +0900

    yattaze

--- tailcomp/tail/tail.ino
+++ tailcomp/tail/tail.ino
@@ -1,12 +1,24 @@
 //tail.ino
 
+int sw;
+int val;
+float alpha1;
+
 void setup() {
   // put your setup code here, to run once:
 	Serial.begin(9600);
+ pinMode(3,INPUT_PULLUP);
+ sw=0;
+ val=1;
+ alpha1=0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  float p1;
+  float p2;
+  float alpha;
 	int valx1=analogRead(0);
 	int valy1=analogRead(1);
 	int valz1=analogRead(2);
@@ -23,8 +35,27 @@ void loop() {
 	float roll1=atan(-gy1/gz1)/M_PI*180;
   float pitch2=atan(gx2/sqrt(square(gy2)+square(gz2)))/M_PI*180;
   float roll2=atan(-gy2/gz2)/M_PI*180;
-  float pitchnew1=(pitch1-3);
-  float pitchnew2=(pitch2+2.1);
+  float pitchnew1=(pitch1-alpha1);
+  float pitchnew2=(pitch2-3);
+  if(val<digitalRead(3)){
+    sw+=1;
+  }
+  
+  if(sw==1){
+    p1=pitchnew1;
+  }
+  
+  if(sw==2){
+    p2=pitchnew1;
+    alpha=(p1+p2)/2;
+  alpha1=alpha;
+   p1=0;
+   p2=0;
+   alpha=0; 
+  sw=0;
+   }
+  val=digitalRead(3);
+  
    /*
   Serial.print(gx);
   Serial.print(",");
@@ -40,14 +71,14 @@ void loop() {
   Serial.print(",");
   Serial.println(roll2);
   */
-/*
+
   Serial.print(pitchnew1);
   Serial.print(",");
   Serial.print(pitchnew2);
   Serial.print(",");
-  */
  
-  Serial.print(pitchnew1-pitchnew2+0.06875*pitchnew2);
+ 
+  Serial.print(pitchnew1-pitchnew2);
   Serial.print(",");
   /*
   Serial.print(roll1);
@@ -55,6 +86,8 @@ void loop() {
   Serial.print(roll2);
   Serial.print(",");
   */
+  Serial.print(sw);
+  Serial.print(",");
   Serial.println(roll1-roll2);
 
 }
